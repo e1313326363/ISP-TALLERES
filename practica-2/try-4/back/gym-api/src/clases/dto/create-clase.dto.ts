21290ae2739382f980566390f226c6811fc4b517
Ian Antonio Velez Moreira <e1313326363@live.uleam.edu.ec>
2023-10-11 00:17:02 -0500

    reorganizacion de los archivos

--- practica-2/try-4/back/gym-api/src/clases/dto/create-clase.dto.ts
+++
@@ -1,8 +0,0 @@
-export class CreateClaseDto {
-    Nombre: string;
-    CoachID: string;
-    MembresiaID: string;
-    Hora: string; // O Date, según tu elección.
-    Duracion: number;
-    CupoMaximo: number;
-}
--- practica-3/src/equipos/dto/create-equipo.dto.ts
+++
@@ -1,7 +0,0 @@
-export class CreateEquipoDto {
-    readonly EquipoID: string;
-    readonly Nombre: string;
-    readonly GymID: string;
-    readonly ClaseID: string;
-    readonly Tipo: string;
-  }
